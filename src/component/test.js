b1059e59fd1de3112f9b8b5e2acb40c2274daca9
CHETAN PUN <chetanpun@CHETANs-MacBook-Pro.local>
2023-08-21 20:07:56 +0545

    changes

---
+++ src/component/test.js
@@ -0,0 +1,14 @@
+
+import Spline from '@splinetool/react-spline';
+
+const Test = () =>{
+
+    return 
+    (
+        <Spline scene="https://prod.spline.design/w34C1yyiXHH5Bad9/scene.splinecode" />
+    )
+}
+
+
+
+export default Test
